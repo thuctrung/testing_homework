7805c79314a790d626e390ae1de98e46766da3d9
unknown <admin@PNVD176.pn.com.vn>
2022-01-13 17:16:04 +0700

    submit folder

---
+++ webdriver/src/test/java/Selenium_webdriver_java_testNG/Selenium_webdriver_java_testNG/AppTest.java
@@ -0,0 +1,11 @@
+package Selenium_webdriver_java_testNG.Selenium_webdriver_java_testNG;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
